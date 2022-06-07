fbf59625473d6e99f45af5340a16cbd8b5e24c28
neburpoots <neburpoots@hotmail.com>
2022-06-07 23:31:25 +0200

    deploy push

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://swaggerbankinholland.herokuapp.com/",
   headers: {
     "Content-Type": "application/json",
   },
--- src/services/setupInterceptors.js
+++ src/services/setupInterceptors.js
@@ -26,7 +26,7 @@ const setup = (store) => {
           originalConfig._retry = true;
           try {
             
-            const rs = await axios.post("http://localhost:8080/api/users/refreshtoken", {
+            const rs = await axios.post("https://swaggerbankinholland.herokuapp.com/", {
               refreshToken: TokenService.getLocalRefreshToken(),
             });
             console.log(rs.data)
